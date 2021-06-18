b43e05aa2b43c7268866e6e7ff3853092e19816e
Raiyan Sharif <raiyan.sharif.1234@gmail.com>
2021-06-18 22:37:14 +0600

    Login and Registration Template with model done

---
+++ AppointmentScheduling/AppointmentScheduling/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using AppointmentScheduling.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public AccountController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
