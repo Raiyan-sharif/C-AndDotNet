2d13e567af38ad6fb7fb34466357e1a90fc26a60
Raiyan Sharif <raiyan.sharif.1234@gmail.com>
2021-06-22 17:42:02 +0600

    development update

---
+++ InAndOut/InAndOut/ActionFilters/ValidationFilterAtribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+using System;
+
+namespace InAndOut.ActionFilters
+{
+    public class ValidationFilterAtribute : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            //var param = context.ActionArguments.SingleOrDefault(p => p.Value is IEntity);
+            throw new NotImplementedException();
+        }
+    }
+}
