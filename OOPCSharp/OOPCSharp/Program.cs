64f6fca7baab52f84ebde9576627b405a8caf2a6
Raiyan Sharif <raiyan.sharif.1234@gmail.com>
2021-06-24 20:37:09 +0600

    using test operation

---
+++ OOPCSharp/OOPCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOPCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] lines = { "This is line1", "This is line2", "This is line3" };
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(@"D:\C-AndDotNet\OOPCSharp\OOPCSharp\some.text"))
+            {
+                foreach(var line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
